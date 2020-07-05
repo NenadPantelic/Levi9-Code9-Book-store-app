f3fbffb3f6e7bcb5153a7d7710c470927303a448
NenadPantelic <nenadpantelickg@gmail.com>
2020-07-05 12:02:28 +0200

    L9C9#002 | Nenad Pantelic | Add service discovery

---
+++ service-discovery/src/test/java/com/levi9/code9/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi9.code9.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
