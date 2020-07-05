d45e55090fe3799f873633c85d00ea5a9535046b
NenadPantelic <nenadpantelickg@gmail.com>
2020-07-05 12:30:31 +0200

    L9C9#003 | Nenad Pantelic | Add auth microservice app

---
+++ auth-service/src/main/java/com/levi9/code9/authservice/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package com.levi9.code9.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
---
+++ auth-service/src/test/java/com/levi9/code9/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi9.code9.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
